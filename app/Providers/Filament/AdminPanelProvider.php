0156bd4dd161fdd800ab2d3de356073cd562a34a
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2026-03-05 03:16:21 +0700

    Added Brand Name for navbar and card login

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -16,9 +16,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use App\Filament\Widgets\ProductCountWidget;
-use App\Filament\Widgets\ArticleCountWidget;
-
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
@@ -28,6 +25,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Woodezig')
             ->colors([
                 'primary' => Color::Amber,
             ])
