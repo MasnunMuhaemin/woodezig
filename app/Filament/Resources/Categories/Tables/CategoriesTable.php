d3ab0f1ffb80403d562920ca3e4f4e38bae54390
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2026-03-05 00:35:13 +0700

    Implementation CRUD Categories

---
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class CategoriesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nama Kategori')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d M Y H:i')
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->label('Diupdate')
+                    ->dateTime('d M Y H:i')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
